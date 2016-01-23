545e6651129ea6760603e2b4404048c998097040
longxiang <longxiang@mucang.cn>
2016-01-24 00:46:43 +0800

    First commit

---
+++ src/main/java/com/lephix/clairetools/Application.java
@@ -0,0 +1,53 @@
+package com.lephix.clairetools;
+
+import com.lephix.clairetools.command.CollectPODTracing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+/**
+ *
+ * Created by longxiang on 16/1/21.
+ */
+@EnableAutoConfiguration
+public class Application implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    Environment environment;
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String commandName = environment.getProperty("command.name");
+        if (StringUtils.isEmpty(commandName)) {
+            LOG.info("No command.name configuration found. Exit.");
+            return;
+        }
+
+        try {
+            switch (commandName) {
+                case "collectPODTracing":
+                    CollectPODTracing collectPODTracing = new CollectPODTracing(environment);
+                    collectPODTracing.run();
+                    break;
+                default:
+                    LOG.info("No command.name matches. Exit.");
+            }
+            LOG.info("{} command has been successfully executed.", commandName);
+        } catch (Exception e){
+            LOG.error("Some error happens. See details.", e);
+        }
+    }
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+}
---
+++ src/main/java/com/lephix/clairetools/command/CollectPODTracing.java
@@ -0,0 +1,122 @@
+package com.lephix.clairetools.command;
+
+import com.google.common.collect.Maps;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.CellReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Map;
+
+/**
+ *
+ * Created by longxiang on 16/1/21.
+ */
+public class CollectPODTracing {
+    private static final Logger LOG = LoggerFactory.getLogger(CollectPODTracing.class);
+
+    Environment environment;
+
+    public CollectPODTracing(Environment environment) {
+        this.environment = environment;
+    }
+
+    public void run() throws Exception {
+        String sourceFolderPath = environment.getProperty("source.folder.path");
+        String sourcePathPattern = environment.getProperty("source.path.pattern");
+        String targetPath = environment.getProperty("target.path");
+        Integer targetWeekCol = CellReference.convertColStringToIndex(
+                environment.getProperty("target.week.col")
+        );
+        Integer languageCodeCol = CellReference.convertColStringToIndex(
+                environment.getProperty("target.lang.code.col", "B")
+        );
+        Integer languageCodeRowStart = environment.getProperty("target.lang.code.row.start", Integer.class, 1);
+
+        File folder = new File(sourceFolderPath);
+        if (!folder.isDirectory()) {
+            LOG.error("folder.path is not a foler.");
+            return;
+        }
+
+        FilenameFilter filenameFilter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.matches(sourcePathPattern);
+            }
+        };
+
+        // Cache all the language code list.
+        Workbook targetWb = WorkbookFactory.create(new FileInputStream(targetPath));
+        Sheet targetSheet = targetWb.getSheetAt(0);
+        Map<String, Integer> codeMap = buildCodeMap(targetSheet, languageCodeCol, languageCodeRowStart);
+
+        // Iterate all files, collect values, write back to the target workbook.
+        for (File file : folder.listFiles(filenameFilter)) {
+            try {
+                Number value = extractValue(file);
+                String sourceLanguageCode = extractLanguageCode(file.getName());
+                int rowNum = findTargetRowNum(codeMap, sourceLanguageCode);
+
+                targetSheet.getRow(rowNum).getCell(targetWeekCol).setCellValue(value.doubleValue());
+            } catch (Exception e) {
+                throw new RuntimeException("Found error during processing file " + file.getAbsolutePath());
+            }
+        }
+
+        // Close and write back the modified data.
+        FileOutputStream fos = new FileOutputStream(targetPath);
+        targetWb.write(fos);
+        fos.close();
+    }
+
+    private Map<String, Integer> buildCodeMap(Sheet sheet, Integer languageCodeCol, int languageCodeRowStart) {
+        Map<String, Integer> codeMap = Maps.newHashMap();
+        for (int rowNum = languageCodeRowStart; rowNum < sheet.getLastRowNum(); rowNum++) {
+            String codeValue = sheet.getRow(rowNum)
+                    .getCell(languageCodeCol)
+                    .getStringCellValue();
+            codeMap.put(codeValue, rowNum);
+        }
+
+        return codeMap;
+    }
+
+    private Number extractValue(File file) throws IOException, InvalidFormatException {
+        Workbook sourceWb = WorkbookFactory.create(file);
+        Sheet sheet = sourceWb.getSheetAt(0);
+        Number cellValue = sheet.getRow(37).getCell(1).getNumericCellValue();
+        sourceWb.close();
+
+        return cellValue;
+    }
+
+    private String extractLanguageCode(String fileName) {
+        fileName = fileName.toLowerCase();
+        Pattern pattern = Pattern.compile(".*ui_(.*)_week.*");
+        Matcher matcher = pattern.matcher(fileName);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+
+        throw new RuntimeException("No language code was extracted from file name = " + fileName);
+    }
+
+    private Integer findTargetRowNum(Map<String, Integer> codeMap, String languageCode) {
+        for (String targetCode : codeMap.keySet()) {
+            if (targetCode.toLowerCase().contains(languageCode)) {
+                return codeMap.get(targetCode);
+            }
+        }
+
+        throw new RuntimeException("No languageCode name matches. languageCode=" + languageCode);
+    }
+
+}
